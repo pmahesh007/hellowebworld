8170b632a97fcc0c3511b31a8b07a66c36ab66ca
jkjit <jkck99@gmail.com>
2020-04-19 12:10:46 +0530

    Modified jenkins-deploy and python file

--- automation/test_landing_page.py
+++ automation/test_landing_page.py
@@ -2,7 +2,7 @@ import selenium.webdriver
 from selenium import webdriver
 from selenium.webdriver import firefox
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.options import Options
 
 import pytest
 
@@ -10,27 +10,25 @@ import pytest
 class TestLandingPage():
 
     def test_check_title(self):
-        print("First step")
-        ffopt = FirefoxOptions()
-        ffopt.add_argument("--headless")
-
-        self.driver = webdriver.Firefox(options=ffopt)
-        print("Second Step")
+        options = Options()
+        options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=options)
         url = "http://localhost:8085"
         driver = self.driver
         driver.get(url)
-        print("THird step")
         print("Web Page title is : {}".format(driver.title))
         assert "Hello World" == driver.title
         driver.close()
-    #
-    # def test_check_content(self):
-    #     self.driver = webdriver.Firefox()
-    #     url = "http://localhost:8085"
-    #     driver = self.driver
-    #     driver.get(url)
-    #     assert "12Apr2020" in driver.page_source
-    #     driver.close()
+
+    def test_check_content(self):
+        options = Options()
+        options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=options)
+        url = "http://localhost:8085"
+        driver = self.driver
+        driver.get(url)
+        assert "12Apr2020" in driver.page_source
+        driver.close()
 
 # if __name__ == '__main__':
 #     main()
